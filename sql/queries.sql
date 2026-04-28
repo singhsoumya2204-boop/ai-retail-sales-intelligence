0ed27af99cf467728926ebf2ad872fc6e4ecb108
soumya singh <singhsoumya.2204@gmail.com>
2026-04-29 01:08:56 +0530

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,19 @@
+-- Revenue by Product
+SELECT ProductID,
+SUM(SalesAmount) AS Revenue
+FROM FactSales
+GROUP BY ProductID;
 
+-- Profit by Region
+SELECT RegionID,
+SUM(SalesAmount - CostAmount) AS Profit
+FROM FactSales
+GROUP BY RegionID;
+
+-- Revenue by Customer Segment
+SELECT C.Segment,
+SUM(F.SalesAmount) Revenue
+FROM FactSales F
+JOIN DimCustomer C
+ON F.CustomerID = C.CustomerID
+GROUP BY C.Segment;
